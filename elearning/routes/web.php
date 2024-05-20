c40f6d07cd6b02530bc09623a5ced0afeabf7ad1
NickyFajaelani525 <nickyfajaelani@gmail.com>
2024-05-19 21:47:43 -1200

    praktikum 10

--- elearning/routes/web.php
+++ elearning/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 //Route
@@ -18,3 +19,6 @@ Route::get('/', function () {
 
 //Dashboard Route
 Route::get('admin/dashboard',[DashboardController::class, 'index']);
+
+//Route untuk menampilkan student
+Route::get('admin/student', [StudentController::class, 'index']);
